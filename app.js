b0239547747166927fa1ec4919e6714623e1b250
Noman Gorchani <nomanaligorchani07@gmail.com>
2024-10-15 14:22:52 +0500

    add new form

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
-let h1=document.querySelector('h1');
-let btn=document.querySelector('button');
-btn.addEventListener("click",function(){
-    h1.innerText="Kya Lusi admi ha tu yar";
-})
+// add new feature form
